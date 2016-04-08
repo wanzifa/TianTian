935a0aaef48bc52fcc3da7e61e34f03a224a33f9
wanzifa <wanzifun@gmail.com>
2016-04-08 19:36:51 +0800

    update 04.07

--- tiantian/tt/models.py
+++ tiantian/tt/models.py
@@ -10,15 +10,41 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class Douban(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    author = models.CharField(max_length=255, blank=True, null=True)
+    author_url = models.CharField(max_length=255, blank=True, null=True)
+    time = models.CharField(max_length=64, blank=True, null=True)
+    book_name = models.CharField(max_length=255, blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    book_url = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'douban'
+
+
 class Guoke(models.Model):
     title = models.CharField(max_length=255, blank=True, null=True)
     author = models.CharField(max_length=50, blank=True, null=True)
     content = models.TextField(blank=True, null=True)
     author_url = models.CharField(max_length=255, blank=True, null=True)
     time = models.CharField(max_length=30, blank=True, null=True)
-    
-    def __unicode__(self):
-        return self.title
+    category = models.CharField(max_length=10, blank=True, null=True)
+
     class Meta:
         managed = False
         db_table = 'guoke'
+
+
+class Huxiu(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    author = models.CharField(max_length=128, blank=True, null=True)
+    author_url = models.CharField(max_length=255, blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    category = models.CharField(max_length=20, blank=True, null=True)
+    time = models.CharField(max_length=48, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'huxiu'
--- tiantian/tt/views.py
+++ tiantian/tt/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from .models import Guoke
+from .models import Guoke,Douban
 
 def index(request):
     context_dict = {}
-    guokrs = Guoke.objects.all()
-    context_dict['guokrs'] = guokrs
+    douban = Douban.objects.all()
+    context_dict['douban'] = douban
     return render(request, 'tt/index.html', context_dict)
     
